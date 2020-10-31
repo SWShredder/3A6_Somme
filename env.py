cd5e7ed6ef5dc5895c372d9b3a1b6d8b8c63c716
0413960 <0413960@cstjean.qc.ca>
2020-10-31 16:47:30 -0400

    FIN env.py

--- env.py
+++ env.py
@@ -6,9 +6,22 @@ Par Yanik Sweeney
 """
 
 
+import getpass
+import os
+# import pprint
+
+
 def main() -> None:
     """Fonction principale"""
-    pass
+    print(f"Il y a {len(os.environ)} variables d'environnement")
+    print(f" - OS: {os.getenv('OS', 'Non défini')}")
+    print(f" - HOME: {os.getenv('HOMEPATH', os.getenv('HOME', 'Non défini'))}")
+
+    print()
+    print("Code portable:")
+    print(f" - {os.name=}")
+    print(f" - user: {getpass.getuser()}")
+    print(f" - home: {os.path.expanduser('~')}")
 
 
 if __name__ == '__main__':
