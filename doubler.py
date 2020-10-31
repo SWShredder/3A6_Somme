229f8cfc873f759c6635554f1ea7f2f7bb36d8b0
0413960 <0413960@cstjean.qc.ca>
2020-10-31 17:44:50 -0400

    FIN doubler.py

---
+++ doubler.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+Programme pour doubler un nombre
+
+Par Yanik Sweeney
+"""
+
+import getpass
+import sys
+import os
+
+
+def main() -> None:
+    """Fonction principale"""
+    verifier_usage()
+    try:
+        nombre = float(sys.argv[1])
+        print('Selon', getpass.getuser(), ':', nombre * 2)
+
+    except ValueError:
+        erreur(f"L'argument '{sys.argv[1]}' n'est pas un nombre")
+
+
+def verifier_usage() -> None:
+    """
+    Vérifie l'usage du programme et en cas d'erreur,
+    affiche un message d'erreur et un usage, puis termine le script
+    """
+    nbargs = len(sys.argv) - 1
+    if nbargs != 1:
+        nom_script = os.path.join('.', os.path.basename(sys.argv[0]))
+        erreur(
+            f"Le script s'attend à recevoir 1 argument, mais vous en avez fourni {nbargs}\n"
+            f"USAGE: {nom_script} nombre"
+        )
+
+
+def erreur(msg: str) -> None:
+    """Affiche un message d'erreur, puis termine le script"""
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
