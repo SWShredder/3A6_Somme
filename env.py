79b728e8710e1ba00c96d357479a56f58f90324d
0413960 <0413960@cstjean.qc.ca>
2020-10-31 16:30:50 -0400

    ajout env pour nouvelle section

---
+++ env.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Programme pour accéder aux variables d'environnement
+
+Par Yanik Sweeney
+"""
+
+
+def main() -> None:
+    """Fonction principale"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
