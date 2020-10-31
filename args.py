8b71a456765bc4c115d9ebd5a1cd276eaebb79b1
0413960 <0413960@cstjean.qc.ca>
2020-10-31 11:06:12 -0400

    Initial commit

---
+++ args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Programme pour afficher les arguments
+
+Par Yanik Sweeney
+"""
+
+
+def main() -> None:
+    """Fonction principale"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
