2bea60f4d602071b91ca3b747f1a8924881467c3
0413960 <0413960@cstjean.qc.ca>
2020-11-16 10:04:08 -0500

    FIN pygrep.py

--- pygrep.py
+++ pygrep.py
@@ -7,14 +7,42 @@ Par Yanik Sweeney
 
 import re
 import sys
+import colorama
+from colorama import Fore
 
 
 def main() -> None:
     """Fonction principale"""
-    input_text = ''.join(sys.stdin.readlines())
+    colorama.init()
+    check_usage()
+    input_text = sys.stdin.readlines()
     regex_arg = sys.argv[1]
-    regex_result = re.search(regex_arg, input_text)
-    print(regex_result)
+    compteur = 0
+    for ligne in input_text:
+        if re.search(regex_arg, ligne):
+            print(ligne, end="")
+            compteur += 1
+    if compteur == 1:
+        afficher_correspondances(f"{compteur} correspondance")
+    elif compteur:
+        afficher_correspondances(f"{compteur} correspondances")
+    else:
+        afficher_correspondances("aucune correspondance")
+
+
+def check_usage() -> None:
+    """Vérifie les arguments et retourne un message d'erreur au besoin"""
+    arg_length = len(sys.argv) - 1
+    if arg_length != 1:
+        print(Fore.RED + "Le script s'attend à recevoir 1 argument,"
+                         " mais vous en avez fourni", arg_length, file=sys.stderr)
+        print(Fore.YELLOW + "USAGE: ./pygrep.py pattern" + Fore.WHITE, file=sys.stderr)
+        sys.exit(1)
+
+
+def afficher_correspondances(message: str) -> None:
+    """Affiche la string de correspondances"""
+    print(Fore.YELLOW + f"\n[ {message} ]" + Fore.WHITE, file=sys.stderr)
 
 
 if __name__ == '__main__':
